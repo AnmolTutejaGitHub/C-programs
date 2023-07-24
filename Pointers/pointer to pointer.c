4403a404dcb2556ae9f165b0b33f50cb26ca4f26
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-07-25 03:35:42 +0530

    Update pointer to pointer.c

--- Pointers/pointer to pointer.c
+++ Pointers/pointer to pointer.c
@@ -8,6 +8,6 @@ main()
 
     printf("%d,%d,%d,%d,%d",a,p,*p,pp,*pp,**pp); //think pp as b so *pp=*b
                                               //p=address of a
-                                              //*p=dereferencing p = a
-                                              //**pp= dereferencing pp two times = dereferencing(dereferencing pp)=dereferencing &p = p =&a
+                                              //*p=dereferencing p = a                                                                 //not *pp=&p note int**p is declaration , pp is variable(pointer) and **pp is dereferencing pp two times 
+                                              //**pp= dereferencing pp two times = dereferencing dereferencing pp =dereferencing dereferencing(&p)(as pp=&p) =dereferencing p = dereferencing &a = a
 }                                    //** is dereferencing here in above comments (remember not pointer declaration that is the confusion here while dereferencing two times)
