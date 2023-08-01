8642a32174333c727828afb78707a327ec59ec42
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-01 07:07:29 +0530

    Create CamelCase.c

---
+++ Hackerrank/Problem Solving/CamelCase.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100000];
+    scanf("%s",str); 
+
+    int count = 0;
+    for (int i=0; str[i] != '\0'; i++) 
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z') 
+        {
+            count++;
+        }
+    }
+    printf("%d",count+1);  //because camelCase
+}
