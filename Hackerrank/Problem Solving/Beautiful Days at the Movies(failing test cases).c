58353d8e42056817341e0d434bbb4721904c96ab
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-02 06:57:57 +0530

    Update Beautiful Days at the Movies(failing test cases).c

--- Hackerrank/Problem Solving/Beautiful Days at the Movies(failing test cases).c
+++ Hackerrank/Problem Solving/Beautiful Days at the Movies(failing test cases).c
@@ -1,21 +1,25 @@
-//Failing Test Cases
 #include<stdio.h>
 #include<math.h>
 
 int main()
 {
-    long long int a,b,k;    //I haven't taken conventional names given by the question
+    long long int a,b,k;
     scanf("%lld %lld %lld",&a,&b,&k);
     
     int arr[b-a+1];  //+1 to include both a and b
     for(int i=0;i<b-a+1;i++)
      arr[i]=a+i;
      
-     int revarr[b-a+1]; //to store reva,reva+1,...,revb
+     int revarr[b-a+1]; //to srore reva,reva+1,...,revb
     
-    for(int i =0;i<b-a+1;i++)  //assigning revarr[] values , its not array reversal but if arr[0]=21 then revarr[0] =12 
+    for(int i =0;i<b-a+1;i++)  //assining revarr[] values , its not array reversal but if arr[0]=21 then revarr[0] =12 
     {
-        revarr[i] = ((arr[i])%10)*10 + (arr[i]/10);
+        //revarr[i] = ((arr[i])%10)*10 + (arr[i]/10);  only doing for 2 digits nos.
+        while(arr[i]>0)
+        {
+      revarr[i] = ((arr[i])%10)*10;
+         arr[i]  =  arr[i]/10;
+        }
 
     }
     int count =0;
@@ -25,5 +29,4 @@ int main()
            count++;
     }
     printf("%d",count);
-  return 0;
 }
