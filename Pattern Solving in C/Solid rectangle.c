584f7e39ecddd2235e88054621d3eb46e03eb6bd
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-15 03:30:53 +0530

    Create Solid rectangle.c

---
+++ Pattern Solving in C/Solid rectangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows:");
+    scanf("%d", &n);
+
+    int m;
+    printf("Enter number of columns:");
+    scanf("%d",&m);
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=m ; j++)
+            printf("*");
+
+            printf("\n");
+    }
+
+    return 0;
+}
