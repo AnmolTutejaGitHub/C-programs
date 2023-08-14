aaddef498619bd123d175fe5a439c7d8211e48d7
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-15 03:28:38 +0530

    Create Number square.c

---
+++ Pattern Solving in C/Number square.c
@@ -0,0 +1,23 @@
+/*Aim to print the following pattern in C
+       1234
+       1234
+       1234
+       1234  */
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    for( int i=1; i<=n ; i++)
+    {
+        for (int j=1; j<=n; j++)
+            printf("%d",j);
+
+        printf("\n");
+    }
+    return 0;
+}
